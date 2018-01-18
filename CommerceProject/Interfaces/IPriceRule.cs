6bdfb29a7855cf1205f7bb13558e8e39dada6f99
kintela <roberto.quintela@gmail.com>
2018-01-18 19:16:26 +0100

    Añadir Interfaces para las reglas de precios

---
+++ CommerceProject/Interfaces/IPriceRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommerceProject.Model;
+
+
+namespace CommerceProject.Interfaces
+{
+    public interface IPriceRule
+    {
+        bool IsMatch(OrderItem item);
+        decimal CalculatePrice(OrderItem item);
+
+    }
+}
